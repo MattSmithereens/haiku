82729e499d0df1567a9c7599808faa2e2886b1c0
Matt Smith and Ryan Murry <ryan@>
2018-08-01 11:38:15 -0700

    add function to count vowels

--- src/haiku.js
+++ src/haiku.js
@@ -1,7 +1,46 @@
 export class Haiku {
   constructor(line1, line2, line3) {
-    this.line1 = line1;
-    this.line2 = line2;
-    this.line3 = line3;
+    this.line1 = line1.split(" ");
+    this.line2 = line2.split(" ");
+    this.line3 = line3.split(" ");
+    this.myLetters = {
+      y: "y",
+      vowels: ["a", "e", "i", "o", "u"]
+    }
+    this.countLine1 = 0;
+    this.countLine2 = 0;
+    this.countLine3 = 0;
   }
+
+  vowelCounter1() {
+    for (let i = 0; i < this.line1.length; i++) {
+      for (let j = 0; j < this.line1[i].length; j++) {
+        if (this.myLetters.vowels.includes(this.line1[i][j])) {
+          this.countLine1++;
+        }
+      }
+    }
+  }
+// LET's GET WET!!!
+  vowelCounter2() {
+    for (let i = 0; i < this.line2.length; i++) {
+      for (let j = 0; j < this.line2[i].length; j++) {
+        if (this.myLetters.vowels.includes(this.line2[i][j])) {
+          this.countLine2++;
+        }
+      }
+    }
+  }
+
+  vowelCounter3() {
+    for (let i = 0; i < this.line3.length; i++) {
+      for (let j = 0; j < this.line3[i].length; j++) {
+        if (this.myLetters.vowels.includes(this.line3[i][j])) {
+          this.countLine3++;
+        }
+      }
+    }
+  }
+
+
 }
